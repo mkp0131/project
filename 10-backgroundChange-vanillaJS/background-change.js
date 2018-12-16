a77861831673c4ee68a9119d27853fc28ebb37b2
mkpMac <mkp0131@naver.com>
2018-12-16 16:11:01 +0900

    .

--- 10-backgroundChange-vanillaJS/background-change.js
+++ 10-backgroundChange-vanillaJS/background-change.js
@@ -1,3 +1,6 @@
-const backgroundName = `background-0${Math.ceil(Math.random()*3)}`
-document.body.style.background = `url(./images/${backgroundName}.jpg) no-repeat 50% 50%`;
-document.body.style.backgroundSize = `cover`;
+function backgroundChange() { 
+	const backgroundName = `background-0${Math.ceil(Math.random()*3)}`
+	document.body.style.background = `url(./images/${backgroundName}.jpg) no-repeat 50% 50%`;
+	document.body.style.backgroundSize = `cover`;
+}
+backgroundChange();
---
+++ 11-getlocation-vanillaJS/getlocation.js
@@ -0,0 +1,22 @@
+const h1Element = document.querySelector("h1");
+const h2Element = document.querySelector("h2");
+const pElement = document.querySelector("p");
+
+navigator.geolocation.getCurrentPosition(function(position) {
+	const latitude = Math.floor(position.coords.latitude);
+	const longitude = Math.floor(position.coords.longitude);
+	const getWeather = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=cb5246d6b85c278a24795a03b2fec637&units=metric`).then(function(res) {
+		return res.json();
+	}).then(function(json) {
+		console.log(json);
+		const weatherStatus = json.weather[0].main;
+		const weatherDescription = json.weather[0].description;
+		const weatherTemp = json.main.temp;
+		h1Element.innerText = weatherStatus;
+		h2Element.innerText= weatherDescription;
+		pElement.innerText = weatherTemp + "℃";
+	});
+
+});
+
+
