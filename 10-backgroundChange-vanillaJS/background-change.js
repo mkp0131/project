f75aa8f7ccf8ea77ea141229a7c55068c9271868
mkpMac <mkp0131@naver.com>
2018-12-16 15:19:57 +0900

    .

---
+++ 10-backgroundChange-vanillaJS/background-change.js
@@ -0,0 +1,3 @@
+const backgroundName = `background-0${Math.ceil(Math.random()*3)}`
+document.body.style.background = `url(./images/${backgroundName}.jpg) no-repeat 50% 50%`;
+document.body.style.backgroundSize = `cover`;
